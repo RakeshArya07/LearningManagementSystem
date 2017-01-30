398a5ea65e009f6f239e17712df825e90f6b9b23
RakeshShastry <rakeshshastry92@gmail.com>
2017-01-30 15:35:36 +0530

    get teacher name by passing the class name

--- src/test/resources/sql/seed.sql
+++ src/test/resources/sql/seed.sql
@@ -6,9 +6,11 @@ TRUNCATE TABLE klasses;
 TRUNCATE TABLE teachers;
 
 set FOREIGN_KEY_CHECKS = 1;
-INSERT into students (id,version,email) values (1,0,'rk@gmail.com');
-INSERT into students (id,version,email) values (2,0,'test@gmail.com');
-INSERT into teachers (id,name,gender,age) values (1,'Chyld','MALE',35),(2,'Andeas','MALE',32);
+INSERT into students (id,version,email) values (1,0,'rk@gmail.com'),
+                                               (2,0,'test@gmail.com');
+
+INSERT into teachers (id,name,gender,age) values (1,'Chyld','MALE',35),
+                                                 (2,'Andeas','MALE',32);
 
 
 INSERT into klasses (id,name,semester,department,fee,teacher_id) values
